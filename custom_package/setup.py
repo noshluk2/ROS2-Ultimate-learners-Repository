30b70588b825c3cfd9115966d29d860e8f9f4cde
luqman <noshluk2@gmail.com>
2021-05-29 18:38:39 +0500

    Adding packages>> custompackage&ros2_turtlesim_deep_dive

---
+++ custom_package/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+package_name = 'custom_package'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='luqman',
+    maintainer_email='noshluk2@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
---
+++ ros2_and_turtlesim_deep_dive/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+package_name = 'ros2_and_turtlesim_deep_dive'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='luqman',
+    maintainer_email='noshluk2@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
