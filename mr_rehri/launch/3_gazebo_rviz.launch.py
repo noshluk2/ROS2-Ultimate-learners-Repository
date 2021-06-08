062068d28fffc60d06722ef6f5c6a8ee9e669089
noshluk2 <noshluk2@gmail.com>
2021-06-08 12:26:09 +0500

    mr_rehri_all plugins added

---
+++ mr_rehri/launch/3_gazebo_rviz.launch.py
@@ -0,0 +1,46 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, ExecuteProcess
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+def generate_launch_description():
+  package_dir=get_package_share_directory('mr_rehri')
+  urdf = os.path.join(package_dir,'3_camera_lidar_gazebo_rviz.urdf')
+  rviz_config_file=os.path.join(package_dir,'3_launch_config.rviz')
+
+  return LaunchDescription([
+    #   publishes TF for links of the robot without joints
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            arguments=[urdf]),
+    #  To publish tf for Joints only links
+        Node(
+            package='joint_state_publisher',
+            executable='joint_state_publisher',
+            name='joint_state_publisher',
+            output='screen',
+            ),
+    # Added Rviz2 as well
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            arguments=['-d',rviz_config_file],
+            output='screen'),
+#  Gazebo related stuff required to launch the robot in simulation
+        ExecuteProcess(
+            cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_factory.so'],
+            output='screen'),
+        Node(
+            package='gazebo_ros',
+            executable='spawn_entity.py',
+            name='urdf_spawner',
+            output='screen',
+            arguments=["-topic", "/robot_description", "-entity", "rehri"])
+  ])
