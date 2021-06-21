7b2741b6eb047c64a88dac39b6e40c8541bd3075
noshluk2 <noshluk2@gmail.com>
2021-06-21 11:30:49 +0500

    Added  plugins to tesla TSC package

--- tsc/launch/world.launch.py
+++ tsc/launch/world.launch.py
@@ -9,7 +9,7 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 def generate_launch_description():
    # main package path
   package_dir=get_package_share_directory('tsc')
-  world_file = os.path.join(package_dir,'car.world')
+  world_file = os.path.join(package_dir,'car_plugins.world')
   pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
   
   return LaunchDescription([
@@ -24,34 +24,4 @@ def generate_launch_description():
                 os.path.join(pkg_gazebo_ros, 'launch', 'gzclient.launch.py')
             ),
         ),
-    #   publishes TF for links of the robot without joints
-        # Node(
-        #     package='robot_state_publisher',
-        #     executable='robot_state_publisher',
-        #     name='robot_state_publisher',
-        #     output='screen',
-        #     arguments=[urdf]),
-    #  To publish tf for Joints only links
-        # Node(
-        #     package='joint_state_publisher',
-        #     executable='joint_state_publisher',
-        #     name='joint_state_publisher',
-        #     output='screen',
-        #     ),
-    # to spawn the robot we added gazebo_ros spawn entity node
-        Node(
-            package='gazebo_ros',
-            executable='spawn_entity.py',
-            name='urdf_spawner',
-            output='screen',
-            arguments=["-topic", "/robot_description", "-entity", "rehri"]
-            ),
-    # The last thing is Rviz with a configuration file
-        # Node(
-        # package='rviz2',
-        # executable='rviz2',
-        # name='rviz2',
-        # arguments=['-d',rviz_config_file],
-        # output='screen'),
-
-  ])
+    ])
